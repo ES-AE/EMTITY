bc593f53423c2aad6bacbb67c8cced8f9e4d5440
ES-AE <48528369+ES-AE@users.noreply.github.com>
2019-05-29 12:51:06 -0600

    Initial commit

---
+++ Menú/Form2.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace Menú
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtDes = new System.Windows.Forms.TextBox();
+            this.txtOrigen = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dgvDatos = new System.Windows.Forms.DataGridView();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(487, 45);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 0;
+            this.btnAgregar.Text = "AGREGAR";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(487, 93);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(75, 23);
+            this.btnMostrar.TabIndex = 1;
+            this.btnMostrar.Text = "MOSTRAR";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(487, 143);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "ELIMINAR";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(157, 45);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(62, 20);
+            this.txtId.TabIndex = 3;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(157, 85);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(214, 20);
+            this.txtNombre.TabIndex = 4;
+            // 
+            // txtDes
+            // 
+            this.txtDes.Location = new System.Drawing.Point(157, 122);
+            this.txtDes.Name = "txtDes";
+            this.txtDes.Size = new System.Drawing.Size(214, 20);
+            this.txtDes.TabIndex = 5;
+            // 
+            // txtOrigen
+            // 
+            this.txtOrigen.Location = new System.Drawing.Point(157, 166);
+            this.txtOrigen.Name = "txtOrigen";
+            this.txtOrigen.Size = new System.Drawing.Size(100, 20);
+            this.txtOrigen.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(124, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(88, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "NOMBRE:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "DESCRIPCIÓN:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(93, 166);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "ORIGEN:";
+            // 
+            // dgvDatos
+            // 
+            this.dgvDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDatos.Location = new System.Drawing.Point(116, 233);
+            this.dgvDatos.Name = "dgvDatos";
+            this.dgvDatos.Size = new System.Drawing.Size(552, 150);
+            this.dgvDatos.TabIndex = 11;
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(593, 45);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(93, 23);
+            this.btnActualizar.TabIndex = 12;
+            this.btnActualizar.Text = "ACTUALIZAR";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(593, 93);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 13;
+            this.btnLimpiar.Text = "LIMPIAR";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.dgvDatos);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtOrigen);
+            this.Controls.Add(this.txtDes);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.btnAgregar);
+            this.Name = "Form2";
+            this.Text = "RECETAS";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtDes;
+        private System.Windows.Forms.TextBox txtOrigen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dgvDatos;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnLimpiar;
+    }
+}
---
+++ Menú/Form2.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Menú
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+        EjercicioEntities ej = new EjercicioEntities();
+
+        private void Mostrar()
+        {
+            dgvDatos.DataSource = ej.Recetas.ToList();
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            Mostrar();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            Recetas re = new Recetas();
+            re.nombre = txtNombre.Text;
+            re.descripcion = txtDes.Text;
+            re.origen = txtOrigen.Text;
+            ej.Recetas.Add(re);
+            ej.SaveChanges();
+            MessageBox.Show("Receta agregada correctamente");
+            Mostrar();
+        }
+        
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            int BorrarId = Convert.ToInt32(txtId.Text);
+            using (EjercicioEntities ObjBorrar = new EjercicioEntities())
+            {
+                Recetas Borrar = (from q in ObjBorrar.Recetas
+                                  where q.id == BorrarId
+                                  select q).First();
+                ObjBorrar.Recetas.Remove(Borrar);
+                ObjBorrar.SaveChanges();
+                MessageBox.Show("Receta eliminada correctamente");
+                Mostrar();
+            }
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            Recetas re = new Recetas();
+            int filtro = Convert.ToInt32(txtId.Text);
+            var update = ej.Recetas.Where(x => x.id == filtro).FirstOrDefault();
+            update.nombre = txtNombre.Text;
+            update.descripcion = txtDes.Text;
+            update.origen = txtOrigen.Text;
+            ej.SaveChanges();
+            MessageBox.Show("Receta actualizada correctamente");
+            Mostrar();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtId.Text = "0";
+            txtNombre.Text = "";
+            txtDes.Text = "";
+            txtOrigen.Text = "";
+            dgvDatos.DataSource = null;
+        }
+    }
+}
